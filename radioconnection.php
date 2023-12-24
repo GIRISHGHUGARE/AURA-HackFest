d9b451ae3b57e01b2314d435d61bd75e96b0e31d
Girish Ghugare <116804758+GIRISHGHUGARE@users.noreply.github.com>
2023-12-24 13:26:59 +0530

    Delete radioconnection.php

--- radioconnection.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $db_name = "signupbuttons";
-    $conn = new mysqli($servername, $username, $password, $db_name);
-    
-?>
